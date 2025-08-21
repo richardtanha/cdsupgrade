6b4efacf62d28eae2e59530dd46d5a8b5cb59cea
Richard Tanha <richard.tanha@sap.com>
2025-08-21 12:00:47 -0400

    added some logging and sample requests

--- srv/cdsupgrade-service.js
+++ srv/cdsupgrade-service.js
@@ -1,4 +1,5 @@
 const cds = require("@sap/cds");
+const LOG = cds.log();
 
 module.exports = async (srv) => {
   // connect to remote service
@@ -23,12 +24,20 @@ module.exports = async (srv) => {
 
   srv.on("READ", EC_FOPayRange, async (req) => {
     try {
-      return await SFSFsrv.send({
+      let res = await SFSFsrv.send({
         query: SELECT.from(EC_FOPayRange).limit(20),
         headers: {
           apikey: process.env.apikey,
         },
       });
+      LOG._info &&
+        LOG.info(
+          "EC_FOPayRange first entry minimumPay value: ",
+          res?.[0]?.minimumPay,
+          " isInteger: ",
+          Number.isInteger(res?.[0]?.minimumPay)
+        );
+      return res;
     } catch (err) {
       req.reject(err);
     }
